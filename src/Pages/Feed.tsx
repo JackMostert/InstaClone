6606b117f4274de6e246ea36b33c1a3d4e3bffdf
Jack Mostert <j_mostert@outlook.com>
2020-01-25 01:43:00 +0000

    updates

--- src/Pages/Feed.tsx
+++ src/Pages/Feed.tsx
@@ -5,6 +5,8 @@ import { Card } from "../Personal-Design-Language/Card/Card";
 import CardImage from "../Personal-Design-Language/CardImage/Index";
 import CardContent from "../Personal-Design-Language/CardContent/Index";
 import CardTitle from "../Personal-Design-Language/CardTitle/Index";
+import Persona from "../Personal-Design-Language/Persona";
+import Icon from "../Personal-Design-Language/Icon";
 
 export interface IFeedProps {
   history: any;
@@ -15,7 +17,6 @@ export interface IFeedState {}
 export default class Feed extends React.Component<IFeedProps, IFeedState> {
   constructor(props: IFeedProps) {
     super(props);
-
     this.state = {};
   }
 
@@ -44,7 +45,276 @@ export default class Feed extends React.Component<IFeedProps, IFeedState> {
           ]}
           onNavLinkClick={url => url && this.props.history.push(url)}
         >
-          <div className="feed-content"></div>
+          <div className="feed-content">
+            <Grid col={5} row={0} colGap="20px" rowGap="20px">
+              <div>
+                <Card
+                  size="fill"
+                  shadowSpread={3}
+                  style={{ borderRadius: "5px" }}
+                >
+                  <CardImage src="https://picsum.photos/400/300" />
+                  <CardTitle size={5}>
+                    <Persona
+                      size={45}
+                      src="https://randomuser.me/api/portraits/men/75.jpg"
+                      text="cezer121"
+                    />
+                  </CardTitle>
+                  <CardContent>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates nemo harum nesciunt magnam, hic minima
+                    accusantium doloremque. Id voluptas aliquid similique
+                    incidunt ipsa necessitatibus. Quo impedit et eligendi ea
+                    aliquam. <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="card-stats">
+                      <Icon
+                        icon="lar la-comments"
+                        fontSize="1.7rem"
+                        text="1234"
+                      />
+                      <Icon icon="lar la-heart" fontSize="1.7rem" text="1234" />
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card
+                  size="fill"
+                  shadowSpread={3}
+                  style={{ borderRadius: "5px" }}
+                >
+                  <CardImage src="https://picsum.photos/400/300" />{" "}
+                  <CardTitle size={5}>
+                    {" "}
+                    <Persona
+                      size={45}
+                      src="https://randomuser.me/api/portraits/men/75.jpg"
+                      text="cezer121"
+                    />
+                  </CardTitle>
+                  <CardContent>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates nemo harum nesciunt magnam, hic minima
+                    accusantium doloremque. Id voluptas aliquid similique
+                    incidunt ipsa necessitatibus. Quo impedit et eligendi ea
+                    aliquam. <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="card-stats">
+                      <Icon
+                        icon="lar la-comments"
+                        fontSize="1.7rem"
+                        text="1234"
+                      />
+                      <Icon icon="lar la-heart" fontSize="1.7rem" text="1234" />
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card
+                  size="fill"
+                  shadowSpread={3}
+                  style={{ borderRadius: "5px" }}
+                >
+                  <CardImage src="https://picsum.photos/200/300" />{" "}
+                  <CardTitle size={5}>
+                    {" "}
+                    <Persona
+                      size={45}
+                      src="https://randomuser.me/api/portraits/men/75.jpg"
+                      text="cezer121"
+                    />
+                  </CardTitle>
+                  <CardContent>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates nemo harum nesciunt magnam, hic minima
+                    accusantium doloremque. Id voluptas aliquid similique
+                    incidunt ipsa necessitatibus. Quo impedit et eligendi ea
+                    aliquam. <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="card-stats">
+                      <Icon
+                        icon="lar la-comments"
+                        fontSize="1.7rem"
+                        text="1234"
+                      />
+                      <Icon icon="lar la-heart" fontSize="1.7rem" text="1234" />
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div>
+                <Card
+                  size="fill"
+                  shadowSpread={3}
+                  style={{ borderRadius: "5px" }}
+                >
+                  <CardImage src="https://picsum.photos/200/300" />{" "}
+                  <CardTitle size={5}>
+                    {" "}
+                    <Persona
+                      size={45}
+                      src="https://randomuser.me/api/portraits/men/75.jpg"
+                      text="cezer121"
+                    />
+                  </CardTitle>
+                  <CardContent>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates nemo harum nesciunt magnam, hic minima
+                    accusantium doloremque. Id voluptas aliquid similique
+                    incidunt ipsa necessitatibus. Quo impedit et eligendi ea
+                    aliquam. <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="card-stats">
+                      <Icon
+                        icon="lar la-comments"
+                        fontSize="1.7rem"
+                        text="1234"
+                      />
+                      <Icon icon="lar la-heart" fontSize="1.7rem" text="1234" />
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card
+                  size="fill"
+                  shadowSpread={3}
+                  style={{ borderRadius: "5px" }}
+                >
+                  <CardImage src="https://picsum.photos/400/300" />{" "}
+                  <CardTitle size={5}>
+                    {" "}
+                    <Persona
+                      size={45}
+                      src="https://randomuser.me/api/portraits/men/75.jpg"
+                      text="cezer121"
+                    />
+                  </CardTitle>
+                  <CardContent>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates nemo harum nesciunt magnam, hic minima
+                    accusantium doloremque. Id voluptas aliquid similique
+                    incidunt ipsa necessitatibus. Quo impedit et eligendi ea
+                    aliquam. <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="card-stats">
+                      <Icon
+                        icon="lar la-comments"
+                        fontSize="1.7rem"
+                        text="1234"
+                      />
+                      <Icon icon="lar la-heart" fontSize="1.7rem" text="1234" />
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div>
+                <Card
+                  size="fill"
+                  shadowSpread={3}
+                  style={{ borderRadius: "5px" }}
+                >
+                  <CardImage src="https://picsum.photos/200/300" />{" "}
+                  <CardTitle size={5}>
+                    {" "}
+                    <Persona
+                      size={45}
+                      src="https://randomuser.me/api/portraits/men/75.jpg"
+                      text="cezer121"
+                    />
+                  </CardTitle>
+                  <CardContent>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates nemo harum nesciunt magnam, hic minima
+                    accusantium doloremque. Id voluptas aliquid similique
+                    incidunt ipsa necessitatibus. Quo impedit et eligendi ea
+                    aliquam. <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="card-stats">
+                      <Icon
+                        icon="lar la-comments"
+                        fontSize="1.7rem"
+                        text="1234"
+                      />
+                      <Icon icon="lar la-heart" fontSize="1.7rem" text="1234" />
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div>
+                <Card
+                  size="fill"
+                  shadowSpread={3}
+                  style={{ borderRadius: "5px" }}
+                >
+                  <CardImage src="https://picsum.photos/200/300" />{" "}
+                  <CardTitle size={5}>
+                    {" "}
+                    <Persona
+                      size={45}
+                      src="https://randomuser.me/api/portraits/men/75.jpg"
+                      text="cezer121"
+                    />
+                  </CardTitle>
+                  <CardContent>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates nemo harum nesciunt magnam, hic minima
+                    accusantium doloremque. Id voluptas aliquid similique
+                    incidunt ipsa necessitatibus. Quo impedit et eligendi ea
+                    aliquam. <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="card-stats">
+                      <Icon
+                        icon="lar la-comments"
+                        fontSize="1.7rem"
+                        text="1234"
+                      />
+                      <Icon icon="lar la-heart" fontSize="1.7rem" text="1234" />
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div>
+                <Card
+                  size="fill"
+                  shadowSpread={3}
+                  style={{ borderRadius: "5px" }}
+                >
+                  <CardImage src="https://picsum.photos/200/300" />{" "}
+                  <CardTitle size={5}>
+                    {" "}
+                    <Persona
+                      size={45}
+                      src="https://randomuser.me/api/portraits/men/75.jpg"
+                      text="cezer121"
+                    />
+                  </CardTitle>
+                  <CardContent>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Voluptates nemo harum nesciunt magnam, hic minima
+                    accusantium doloremque. Id voluptas aliquid similique
+                    incidunt ipsa necessitatibus. Quo impedit et eligendi ea
+                    aliquam.
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="card-stats">
+                      <Icon
+                        icon="lar la-comments"
+                        fontSize="1.7rem"
+                        text="1234"
+                      />
+                      <Icon icon="lar la-heart" fontSize="1.7rem" text="1234" />
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            </Grid>
+          </div>
         </Page>
       </div>
     );
